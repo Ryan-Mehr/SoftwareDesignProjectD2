7fb37f4abb8a4bd565a1cdb83099457fb5b32d4e
Anastasia Filipoiu <anastasia.filipoiu@gmail.com>
2026-03-20 17:02:54 -0400

    current PvP battle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,10 +11,16 @@ module org.example.softwaredesignprojectd2 {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires java.sql;
-    requires flyway.core;
     requires jbcrypt;
 
+    // Open ALL your packages to JavaFX
     opens GUI to javafx.fxml;
+    opens Classes to javafx.base, javafx.fxml;        // ADD THIS
+    opens Classes.Heros to javafx.base, javafx.fxml;  // ADD THIS
+    opens GlobalVariables to javafx.base, javafx.fxml; // ADD THIS
     opens db.migration;
+
     exports GUI;
+    exports Classes;
+    exports Classes.Heros;
 }
